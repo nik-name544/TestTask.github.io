e6386ec38373a96bbe8d61100fd978e2a9e0a9e8
nik-name544 <revenkonik30@gmail.com>
2023-04-05 17:21:32 +0200

    Updates

--- src/components/ComponentWrapper/componentWrapperStyle.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const ComponentWrapper = styled.div`
-  padding: 0 32px;
-  border-bottom: 1px solid #e9ebf2;
-  border: ${(p) => p.withoutBorder && "none"};
-`;
